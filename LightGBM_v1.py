cb586fb3d0a0c94f4a7e784555cc84a09b3de7e5
Ozkan Batumoglu <batumoglu@hotmail.com>
2018-06-05 19:31:38 +0300

    KFold Added

--- LightGBM_v1.py
+++ LightGBM_v1.py
@@ -9,6 +9,8 @@ Created on Mon May 28 18:40:02 2018
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
+import gc
 
 """ Custom functions for data gathering """
 import Gather_Data
@@ -17,11 +19,32 @@ import Gather_Data
 from lightgbm import LGBMClassifier
 
 """ Gather data: ApplicationOnly, ApplicationBuroAndPrev, AllData """
-train_X, test_X, train_Y = Gather_Data.AllData(reduce_mem=False)
+train_X, test_X, train_Y = Gather_Data.ApplicationOnly(reduce_mem=False)
 
-clf = LGBMClassifier()
-clf.fit(train_X, train_Y, eval_metric='auc')
-train_preds = clf.predict(train_X)
-test_preds = clf.predict_proba(test_X)[:,1]
+oof_preds = np.zeros(train_X.shape[0])
+sub_preds = np.zeros(test_X.shape[0])
 
-print('AUC : %.6f' % roc_auc_score(train_Y, train_preds))
+folds = KFold(n_splits=5, shuffle=True, random_state=1453)
+
+for n_fold, (trn_idx, val_idx) in enumerate(folds.split(train_X)):
+    trn_X, trn_y = train_X.iloc[trn_idx], train_Y.iloc[trn_idx]
+    val_X, val_y = train_X.iloc[val_idx], train_Y.iloc[val_idx]
+    
+    clf = LGBMClassifier()
+    clf.fit(trn_X, trn_y)
+    oof_preds[val_idx] = clf.predict_proba(val_X)[:,1]
+    sub_preds += clf.predict_proba(test_X)[:,1] / folds.n_splits
+    
+    del clf, trn_X, trn_y, val_X, val_y
+    gc.collect()
+
+print('AUC : %.6f' % roc_auc_score(train_Y, oof_preds))
+sub_preds.to_csv('ApplicationOnly_LightGBM_v1.csv')
+
+"""
+random_state    =1453
+train AUC       =0.756493
+test AUC        =
+LigGBM Parameters:
+Null
+"""
