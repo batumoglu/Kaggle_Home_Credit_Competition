a587fc64365e8eb5a21809f6989a22196ed47fd4
Ozkan Batumoglu <batumoglu@sabanciuniv.edu>
2018-07-03 07:37:35 +0300

    Added submission

--- XGB_CV_v1.py
+++ XGB_CV_v1.py
@@ -14,7 +14,7 @@ import xgboost as xgb
 import graphviz
 
 # Gather Data
-train_X, test_X, train_Y = Dataset.Load('ApplicationOnly')
+train_X, test_X, train_Y = Dataset.Load('AllData_v3')
 
 # Convert data to DMatrix
 dtrain = xgb.DMatrix(train_X, train_Y)
@@ -41,7 +41,7 @@ cv_results = xgb.cv(params=params,
                     nfold=5,
                     maximize=True,
                     early_stopping_rounds=10,
-                    verbose_eval=2)
+                    verbose_eval=1)
 
 # Get best number of iterations
 num_boost_rounds = len(cv_results)
@@ -54,9 +54,17 @@ model   = xgb.train(params=params,
                     maximize=True,
                     verbose_eval=10)
 
+# Prediction
+sub_preds = model.predict(dtest)
+sub = pd.read_csv('../input/sample_submission.csv')
+sub['TARGET'] = sub_preds
+sub.to_csv('AllData_v3_XGB_CV_v1.csv', index=False)
+
+"""
 # Plot importance
 xgb.plot_importance(model,
                     max_num_features=20)
 
 # Plot tree
 xgb.plot_tree(model)
+"""
