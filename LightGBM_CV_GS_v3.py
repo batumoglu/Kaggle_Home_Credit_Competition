40f007a10d08ac74d6e2882d9ba895a15537631b
Ozkan Batumoglu <batumoglu@hotmail.com>
2018-07-06 00:07:37 +0300

    Adding train, train_preds, model_save

--- LightGBM_CV_GS_v3.py
+++ LightGBM_CV_GS_v3.py
@@ -38,22 +38,41 @@ cv_params = {
     "verbose_eval"          : 10
 }
 
-param_grid = {"num_leaves"    : range(20,101,20)}
+param_grid = {"num_leaves"    : range(20,41,20)}
 lgbm = LGBM(params)
 gs_results, params = lgbm.gridsearch(param_grid, cv_params)
 gs_summary = gs_results
 
-param_grid = {"max_depth"    : range(3,8,1)}
+param_grid = {"max_depth"    : range(3,5,1)}
 lgbm = LGBM(params)
 gs_results, params = lgbm.gridsearch(param_grid, cv_params)
 gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
 
+print('All Iterations')
 display(gs_summary)
 print('Best parameters: ')
-display(gs_results.loc[gs_results['result'].idxmax()])
+best_cv = gs_results.loc[gs_results['result'].idxmax()]
+display(best_cv)
 
 profile.End()
 
 gs_summary.to_csv('../AllData_v3_LGBM_GS.csv')
 
+# Generate model by best iteration
+model   = lgb.train(params=params,
+                    train_set=lgb_train,
+                    num_boost_round=best_cv[1],
+                    verbose_eval=1)
 
+model.savel_model('AllData_v3_LGBM_Model', num_iteration=best_cv[1])
+
+train_preds = model.predict(train_X)
+data = pd.read_csv('../input/application_train.csv')
+data = data['SK_ID_CURR']
+data['preds'] = train_preds
+data.to_csv('AllData_v3_LGBM_TrainPreds.csv', index=False)
+
+sub_preds = model.predict(test_X)
+sub = pd.read_csv('../input/sample_submission.csv')
+sub['TARGET'] = sub_preds
+sub.to_csv('AllData_v3_LGBM_Score.csv', index=False)
