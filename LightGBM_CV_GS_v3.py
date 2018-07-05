cb8b48e02c25787febbe5a6551c0c364797e3703
Ozkan Batumoglu <batumoglu@hotmail.com>
2018-07-06 00:33:22 +0300

    Parameters, saving_model, train_score, predictions added

--- LightGBM_CV_GS_v3.py
+++ LightGBM_CV_GS_v3.py
@@ -33,18 +33,44 @@ params = {'task'                    :'train',
 # Parameters that are to be supplied to cross-validation
 cv_params = {
     "train_set"             : lgb_train,
-    "num_boost_round"       : 100,
-    "nfold"                 : 3,
-    "early_stopping_rounds" : 20,
+    "num_boost_round"       : 1000,
+    "nfold"                 : 5,
+    "early_stopping_rounds" : 30,
     "verbose_eval"          : 10
 }
 
-param_grid = {"num_leaves"    : range(20,41,20)}
+# Step 1
+param_grid = {"num_leaves"    : range(10,101,10)}
 lgbm = LGBM(params)
 gs_results, params = lgbm.gridsearch(param_grid, cv_params)
 gs_summary = gs_results
 
-param_grid = {"max_depth"    : range(3,5,1)}
+# Step 2
+param_grid = {"max_depth"    : range(3,10,1)}
+lgbm = LGBM(params)
+gs_results, params = lgbm.gridsearch(param_grid, cv_params)
+gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
+
+# Step 3
+param_grid = {"min_data_in_leaf"    : range(10,81,10)}
+lgbm = LGBM(params)
+gs_results, params = lgbm.gridsearch(param_grid, cv_params)
+gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
+
+# Step 4
+param_grid = {"lambda_l1"    : [i/10.0 for i in range(0,8)]}
+lgbm = LGBM(params)
+gs_results, params = lgbm.gridsearch(param_grid, cv_params)
+gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
+
+# Step 5
+param_grid = {"lambda_l2"    : [i/10.0 for i in range(0,8)]}
+lgbm = LGBM(params)
+gs_results, params = lgbm.gridsearch(param_grid, cv_params)
+gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
+
+# Step 6
+param_grid = {"scale_pos_weight"    : [92/8, 1]}
 lgbm = LGBM(params)
 gs_results, params = lgbm.gridsearch(param_grid, cv_params)
 gs_summary = pd.concat([gs_summary, gs_results], ignore_index=True)
@@ -57,6 +83,7 @@ display(best_cv)
 
 profile.End()
 
+# Save CV process
 gs_summary.to_csv('../AllData_v3_LGBM_GS.csv')
 
 # Generate model by best iteration
@@ -65,15 +92,18 @@ model   = lgb.train(params=params,
                     num_boost_round=best_cv[1],
                     verbose_eval=1)
 
+# Save model for possible coded ensemble
 model.save_model('../AllData_v3_LGBM_Model', num_iteration=best_cv[1])
 
+# Generate train prediction for future ensemble
 train_preds = model.predict(train_X)
 data = pd.read_csv('../input/application_train.csv')
-data = data['SK_ID_CURR']
 data['preds'] = train_preds
+data = data[['SK_ID_CURR', 'preds']]
 data.to_csv('../AllData_v3_LGBM_TrainPreds.csv', index=False)
 
+# Generate sub prediction for Kaggle
 sub_preds = model.predict(test_X)
 sub = pd.read_csv('../input/sample_submission.csv')
 sub['TARGET'] = sub_preds
-sub.to_csv('../AllData_v3_LGBM_Score.csv', index=False)
+sub.to_csv('../AllData_v3_LGBM_Preds.csv', index=False)
