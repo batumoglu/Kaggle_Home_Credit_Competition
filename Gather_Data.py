05ae3c66c446a7ca964331fc402767046652ea53
Ozkan Batumoglu <batumoglu@sabanciuniv.edu>
2018-08-06 00:49:36 +0300

    AllData_v4 almost ready

--- Gather_Data.py
+++ Gather_Data.py
@@ -75,8 +75,8 @@ def AllData_v4(reduce_mem=True):
     app_data = GatherTables.generateAppFeatures_v4(app_data)
     
     merged_df = GatherTables.handlePrev_v4(app_data)
-    merged_df = GatherTables.handleCreditCard_v2(merged_df)
-    merged_df = GatherTables.handleBuro_v2(merged_df)
+    merged_df = GatherTables.handleCreditCard_v4(merged_df)
+    merged_df = GatherTables.handleBuro_v4(merged_df)
     merged_df = GatherTables.handleBuroBalance_v2(merged_df)
     merged_df = GatherTables.handlePosCash_v2(merged_df)
     merged_df = GatherTables.handleInstallments_v2(merged_df)
